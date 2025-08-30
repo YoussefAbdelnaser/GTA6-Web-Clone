cd9bf7b7beba44b999f75ef513b3d28b01f5147f
YoussefAbdelnaser <youssefabdelnasersobhy@yahoo.com>
2025-08-30 22:19:55 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
 import PostCard from "./sections/PostCard";
 import Final from "./sections/Final";
+import Outro from "./sections/Outro";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -24,8 +25,8 @@ const App = () => {
       <Lucia />
 
       <PostCard />
-
       <Final />
+      <Outro />
     </main>
   );
 };
