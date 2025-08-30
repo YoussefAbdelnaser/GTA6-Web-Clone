b38562c7a2ee3d5ce84abafb20e2f35b416d99b7
YoussefAbdelnaser <youssefabdelnasersobhy@yahoo.com>
2025-08-30 09:11:01 +0300

    added first video

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Hero from "./sections/Hero";
 import NavBar from "./sections/NavBar";
+import FirstVideo from "./sections/FirstVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +11,7 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <FirstVideo />
     </main>
   );
 };
